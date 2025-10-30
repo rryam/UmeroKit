925683c934fa6e7e98da926f2a5530c643ae9c78
Rudrank Riyam <rudrankriyam@gmail.com>
2025-10-30 20:20:10 +0530

    Remove build docs and GitHub Actions

--- build_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-swift package --allow-writing-to-directory ./docs \
-generate-documentation --target UmeroKit \
---disable-indexing \
---transform-for-static-hosting \
---hosting-base-path UmeroKit \
---output-path ./docs
